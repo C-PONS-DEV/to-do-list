92f39aec7b30dbb72c649b241e417908a4380a93
Clément PONS <70891308+C-PONS-DEV@users.noreply.github.com>
2021-04-06 18:44:51 +0200

    Ajout des fichiers

---
+++ src/Containers/App/App.js
@@ -0,0 +1,137 @@
+import React, { useState, useRef, useEffect } from 'react';
+import classes from './App.module.css';
+import Task from '../../Components/Task/Task';
+import axios from '../../axios-firebase';
+
+function App() {
+
+  // States
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
+
+  // Ref
+  const inputRef = useRef('');
+
+  // Cycle de vie
+  useEffect(() =>{
+    inputRef.current.focus();
+
+    // Axios
+    axios.get('./taches.json')
+      .then(response =>{
+        const tasksArray = [];
+        for(let key in response.data){
+          tasksArray.push({
+              ...response.data[key],
+              id: key
+          });
+        }
+        setTasks(tasksArray);
+      })
+      .catch(error =>{
+        console.log(error);
+      });
+  }, []);
+
+  // Fonctions
+  const removeClickedHandler = index => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+
+    axios.delete('/taches/'+ tasks[index].id +'.json',tasks[index])
+    .then(response =>{
+      console.log(response);
+    })
+    .catch(error =>{
+      console.log(error);
+    });
+  }
+
+  const doneClickedHandler = index => {
+    const newTasks = [...tasks];
+    newTasks[index].done = !tasks[index].done;
+    setTasks(newTasks);
+
+    // Axios
+    axios.put('/taches/'+ tasks[index].id +'.json',tasks[index])
+    .then(response =>{
+      console.log(response);
+    })
+    .catch(error =>{
+      console.log(error);
+    });
+  }
+
+  const submittedTaskHandler = event => {
+    event.preventDefault();
+
+    const newTask = {
+      content: input,
+      done: false
+    }
+    setTasks([...tasks, newTask]);
+    setInput('');
+
+
+    // Axios
+    axios.post('/taches.json', newTask)
+    .then(reponse =>{
+      console.log(reponse);
+    })
+    .catch(error =>{
+      console.log(error);
+    });
+  }
+
+  const changedFormHandler = event => {
+    setInput(event.target.value);
+  }
+
+  // Variables
+  let tasksDisplayed = tasks.map((task, index) => (
+    <Task
+      done={task.done}
+      content={task.content}
+      key={index}
+      removeClicked={() => removeClickedHandler(index)}
+      doneClicked={() => doneClickedHandler(index)}
+    />
+  ));
+  // let donedTasks = tasks.filter(task => task.done)
+  //   .map((filteredTask, index) => (
+  //     <Task
+  //       done={filteredTask.done}
+  //       content={filteredTask.content}
+  //       key={index}
+  //       removeClicked={() => removeClickedHandler(index)}
+  //       doneClicked={() => doneClickedHandler(index)}
+  //     />
+  // ));
+
+  return (
+    <div className={classes.App}>
+      <header>
+        <span>TO-DO</span>
+      </header>
+
+      <div className={classes.add}>
+        <form onSubmit={(e) => submittedTaskHandler(e)}>
+          <input
+            type="text"
+            value={input}
+            ref={inputRef}
+            onChange={(e) => changedFormHandler(e)}
+            placeholder="Que souhaitez-vous ajouter ?" />
+          <button type="submit">
+            Ajouter
+          </button>
+        </form>
+      </div>
+
+      {tasksDisplayed}
+    </div>
+  );
+}
+
+export default App;
